53f24aa4028b87d47c7b3c61a1ba55f0b8d59edf
Dave <Dave7995@gmail.com>
2021-05-27 11:16:08 -0400

    fixed EPV Chart

--- trakberry/views_db.py
+++ trakberry/views_db.py
@@ -11,8 +11,8 @@ def net1(request):
 	request.session["local_switch"] = 0
 	request.session["local_toggle"] = "/trakberry"
 	# ###this
-	# request.session["local_switch"] = 1
-	# request.session["local_toggle"] = ""
+	request.session["local_switch"] = 1
+	request.session["local_toggle"] = ""
 	return
 
 # Methods for opening database for all and returning db and cur
@@ -49,16 +49,16 @@ def db_open():
 
 # This will set the correct database based on a error acknowledgement.	# return db, cursor
 # It will aslo initialize local_toggle which is used for the workaround on templates
-
+1	
 
 
 def db_set(request):  # Module to set DB settings to the one that works.  Whether local or Server
-	# db = MySQLdb.connect(host="10.4.1.224",user="dg417",passwd="dg",db='prodrptdb')
-	# cursor = db.cursor()
-	# sql = "SELECT * from testtest" 
-	# cursor.execute(sql)
-	# tmp2 = cursor.fetchall()
-	# return db, cursor
+	db = MySQLdb.connect(host="10.4.1.224",user="dg417",passwd="dg",db='prodrptdb')
+	cursor = db.cursor()
+	sql = "SELECT * from testtest" 
+	cursor.execute(sql)
+	tmp2 = cursor.fetchall()
+	return db, cursor
 
  # update
 
